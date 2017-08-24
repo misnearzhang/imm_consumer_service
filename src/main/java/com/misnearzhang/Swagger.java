ff8d3ff721b968cf59a73675cba1ae6f3b4a6072
zhanglong <123456>
2017-08-24 22:18:12 +0800

    swagger

---
+++ src/main/java/com/misnearzhang/Swagger.java
@@ -0,0 +1,38 @@
+package com.misnearzhang;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.misnearzhang.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("swagger ，默认页面")
+                .description("测试")
+                .termsOfServiceUrl("localhost")
+                .contact(new Contact("张龙","http://192.168.1.1","misnearzhang@gmail.com"))
+                .version("1.0")
+                .build();
+    }
+
+}
--- src/main/java/com/misnearzhang/controller/UserController.java
+++ src/main/java/com/misnearzhang/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.misnearzhang.controller;
 
 import com.misnearzhang.grpc.consumer.OrderServiceConsu;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,13 +19,16 @@ public class UserController {
     @Autowired
     OrderServiceConsu orderServiceConsu;
 
-
+    @ApiOperation(value = "index",notes = "一个测试的东西")
     @RequestMapping("/")
     public Object get(){
         return orderServiceConsu.getUserInfo("nihk");
     }
 
-    @RequestMapping("/make-order")
+
+    @ApiOperation(value = "makeOrder",notes = "一个测试的东西")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
+    @RequestMapping(value = "/make-order" , method = RequestMethod.POST)
     public Object makeOrder(){
         return orderServiceConsu.makeOrder();
     }
